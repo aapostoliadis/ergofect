c4c03c0fb4dae42c39e337735ded9b0ff2cfd68e
Akis Apostoliadis <aapostoliadis@gmail.com>
2026-08-28 18:04:37 +0300

    Add Apollo website tracking

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -10,6 +10,16 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Archivo:wght@300;400;500;600;700&family=IBM+Plex+Mono:wght@400;500&display=swap"
           rel="stylesheet"
         />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              function initApollo(){var n=Math.random().toString(36).substring(7),o=document.createElement("script");
+                o.src="https://assets.apollo.io/micro/website-tracker/tracker.iife.js?nocache="+n,o.async=!0,o.defer=!0,
+                o.onload=function(){window.trackingFunctions.onLoad({appId:"6a9194244fedf0000c31ff6b"})},
+                document.head.appendChild(o)}initApollo();
+            `,
+          }}
+        />
       </Head>
       <body className="min-h-screen font-sans bg-bone text-ink selection:bg-cobalt/[0.16] selection:text-ink overflow-x-hidden">
         <Main />
